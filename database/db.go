4eaa462aeaa92d523d0079fdb6575233e2d2592f
Ruan Vinícius <contatoruanvinicius@gmail.com>
2023-11-04 15:50:30 -0300

    add gorm add students migrations

--- database/db.go
+++ database/db.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"gin-rest-api/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func Connect() {
+
+	dsn := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err := gorm.Open(postgres.Open(dsn))
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB.AutoMigrate(&models.Student{})
+}
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"gin-rest-api/database"
 	"gin-rest-api/routes"
 )
 
 func main() {
+	database.Connect()
 	routes.HandleRequests()
 }
