39ab7e6620f67328cacc7869feadae13b4ff4f87
Ruan Vinícius <contatoruanvinicius@gmail.com>
2023-11-05 15:49:23 -0300

    add edit, search and find method

--- controllers/controller.go
+++ controllers/controller.go
@@ -4,6 +4,7 @@ import (
 	"gin-rest-api/database"
 	"gin-rest-api/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func ShowAllStudents(c *gin.Context) {
@@ -21,5 +22,53 @@ func CreateStudent(c *gin.Context) {
 	}
 
 	database.DB.Create(&student)
+	c.JSON(200, student)
+}
+
+func GetStudentById(c *gin.Context) {
+	var student models.Student
+	id := c.Param("id")
+	database.DB.First(&student, id)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student  not found"})
+		return
+	}
+	c.JSON(200, student)
+}
+
+func DeleteStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Param("id")
+
+	database.DB.Delete(&student, id)
+	c.JSON(http.StatusOK, gin.H{"data": "Aluno deletado com sucesso"})
+
+}
+
+func EditStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
+
+}
+
+func SearchByCpf(c *gin.Context) {
+	var student models.Student
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Student{CPF: cpf}).First(&student)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student  not found"})
+		return
+	}
 	c.JSON(200, student)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,10 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/api/students", controllers.ShowAllStudents)
+	r.GET("/api/students/:id", controllers.GetStudentById)
 	r.POST("/api/students", controllers.CreateStudent)
+	r.DELETE("/api/students/:id", controllers.DeleteStudent)
+	r.PATCH("/api/students/:id", controllers.EditStudent)
+	r.GET("/api/students/search/cpf/:cpf", controllers.SearchByCpf)
 	r.Run()
 }
